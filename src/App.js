b70e593dfbcd4f5d90319c4b520b6849c6831441
Md Jahidul Islam <iamjahid697@gmail.com>
2021-05-22 23:03:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers]= useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+    .catch(error=>console.log(error))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Users:{users.length}</h1>
+      <ul>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }</ul>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
